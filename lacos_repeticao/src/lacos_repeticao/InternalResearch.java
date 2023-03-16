281d1e4b410f24d75e1746a6ddbf5bc65a962c21
flavws <persona.fmaria@gmail.com>
2023-03-16 14:41:56 -0300

    alteracao exercicio

--- lacos_repeticao/src/lacos_repeticao/InternalResearch.java
+++ lacos_repeticao/src/lacos_repeticao/InternalResearch.java
@@ -5,22 +5,43 @@ import java.util.Scanner;
 public class InternalResearch {
 
 	public static void main(String[] args) {
-		int idade, sexo, categoria;
-		String continuar="S";
+		int idade, sexo, categoria, contBackend = 0, contMulheres = 0, contHomens = 0, contFull = 0;
+		char continuar='S';
 		
 		Scanner leia = new Scanner(System.in);
 
-		while(continuar.equals("S")) {
+		while(continuar != 'N') {
 			System.out.println("Digite a idade:");
 			idade = leia.nextInt();
 			System.out.println("Digite o sexo:");
 			sexo = leia.nextInt();
 			System.out.println("Digite a categoria:");
-			idade = leia.nextInt();
+			categoria = leia.nextInt();
 			
-			System.out.println("Deseja continuar a leitura de dados?");
-			continuar = leia.next();
+			if(categoria == 1) {
+				contBackend++;
+			} else if (sexo ==2 && categoria == 2) {
+				contMulheres++;
+			} else if (sexo == 1 && categoria == 3 && idade > 40) {
+				contHomens++;
+			} else if (sexo == 2 && categoria == 4 && idade < 30) {
+				contFull++;
+			}
+			
+			if(sexo <= 3 && categoria <= 4) {
+				System.out.println("Deseja continuar a leitura de dados?");
+				continuar = leia.next().charAt(0);
+			} else {
+				System.out.println("Selecione uma opção válida"); 
+			}
+			
 		}
+		
+		System.out.println("Total de pessoas desenvolvedoras Backend: " + contBackend);
+		System.out.println("Total de mulheres desenvolvedoras Frontend: " + contMulheres);
+		System.out.println("Total de homens desenvolvedores Mobile maiores de 40 anos: " + contHomens);
+		System.out.println("Total de mulheres desenvolvedoras FullStack menores de 30 anos: " + contFull);
+
 	}
 
 }
